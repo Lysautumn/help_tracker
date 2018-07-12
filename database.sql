c6f4afc5dc4a0ba8299c390dd05e448e1a388057
Ally <ally@primeacademy.io>
2018-07-12 09:53:54 -0500

    update database.sql file

--- database.sql
+++ database.sql
@@ -3,3 +3,39 @@ CREATE TABLE person (
     username VARCHAR (80) UNIQUE NOT NULL,
     password VARCHAR (1000) NOT NULL
 );
+
+ALTER TABLE person
+	ADD name VARCHAR (80) NOT NULL
+;
+
+CREATE TABLE cohorts (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(20) NOT NULL UNIQUE
+);
+
+CREATE TABLE assignments (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE topics (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(50) NOT NULL UNIQUE
+);
+
+CREATE TABLE students (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(80) NOT NULL UNIQUE
+);
+
+CREATE TABLE events (
+	id SERIAL PRIMARY KEY,
+	date timestamp NOT NULL,
+	title VARCHAR(80) NOT NULL,
+	instructor_id INT REFERENCES "person" NOT NULL,
+	cohort_id INT REFERENCES "cohorts" NOT NULL,
+	assignment_id INT REFERENCES "assignments" NOT NULL,
+	topic_id INT REFERENCES "topics",
+	student_id INT REFERENCES "students" NOT NULL,
+	notes TEXT
+);
