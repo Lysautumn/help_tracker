09fea437886b1dba5a4a0a98523510affacdffc6
Ally <ally@primeacademy.io>
2018-07-26 11:29:12 -0500

    styling and history table only displays completed events

--- src/components/HistoryTable/HistoryTable.js
+++ src/components/HistoryTable/HistoryTable.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
+import Moment from 'react-moment';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableRow from '@material-ui/core/TableRow';
+import Button from '@material-ui/core/Button';
+import Delete from '@material-ui/icons/Delete';
+import More from '@material-ui/icons/More';
+import red from '@material-ui/core/colors/red';
+import teal from '@material-ui/core/colors/teal';
 
 import { USER_ACTIONS } from '../../redux/actions/userActions';
 
@@ -13,14 +19,20 @@ const mapStateToProps = state => ({
   event: state.event,
 });
 
+const styles = {
+  deleteButton: {
+    backgroundColor: red[400],
+  },
+  moreButton: {
+    backgroundColor: teal[400],
+  },
+}
+
 
 
 class HistoryTable extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      historyList: []
-    };
   }
   componentDidMount() {
     this.props.dispatch({ type: USER_ACTIONS.FETCH_USER });
@@ -40,7 +52,19 @@ class HistoryTable extends Component {
         <Table>
           <TableBody>
           {this.props.event.eventList && this.props.event.eventList.eventName.map( (event) => {
-        return <TableRow key={event.name}><TableCell>{event.date}</TableCell><TableCell>{event.title}</TableCell><TableCell>{event.student}</TableCell><TableCell>{event.instructor}</TableCell></TableRow>})}
+            if (event.completed == true) {
+              return ( 
+                <TableRow key={event.id}>
+                  <TableCell><Moment format="MM/DD/YYYY hh:mm a">{event.date}</Moment></TableCell>
+                  <TableCell>{event.title}</TableCell>
+                  <TableCell>{event.student}</TableCell>
+                  <TableCell>{event.instructor}</TableCell>
+                  <TableCell><Button style={styles.moreButton}><More /></Button></TableCell>
+                  <TableCell><Button style={styles.deleteButton}><Delete /></Button></TableCell>
+                </TableRow>
+              )
+            }
+            })}
           </TableBody>
         </Table>
  
